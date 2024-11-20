7cf723f7abc867f986be21aeee41a1e664a7e260
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2024-11-20 22:44:29 +0100

    Solution sketch for E-1.

--- in3120/naivebayesclassifier.py
+++ in3120/naivebayesclassifier.py
@@ -33,11 +33,12 @@ class NaiveBayesClassifier:
         # i.e., c maps to log(Pr(c)).
         self.__priors: Dict[str, float] = {}
 
-        # Maps a category c and a term t to the logarithm of its conditional probability,
-        # i.e., (c, t) maps to log(Pr(t | c)).
-        self.__conditionals: Dict[str, Dict[str, float]] = {}
+        # Maps a category c and a term t to the logarithm of its category-conditioned
+        # posterior probability, i.e., (c, t) maps to log(Pr(t | c)).
+        self.__posteriors: Dict[str, Dict[str, float]] = {}
 
-        # Maps a category c to the denominator used when doing Laplace smoothing.
+        # Maps a category c to the denominator used when doing Laplace smoothing for
+        # the posterior probabilities.
         self.__denominators: Dict[str, int] = {}
 
         # Train the classifier, i.e., estimate all probabilities.
@@ -50,20 +51,40 @@ class NaiveBayesClassifier:
         Estimates all prior probabilities (or, rather, log-probabilities) needed for
         the naive Bayes classifier.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        # Maximum likelihood estimate.
+        total_count = sum(map(len, training_set.values()))
+        self.__priors = {category: math.log(corpus.size() / total_count) for category, corpus in training_set.items()}
 
     def __compute_vocabulary(self, training_set, fields) -> None:
         """
         Builds up the overall vocabulary as seen in the training set.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        # We're doing simple add-one (Laplace) smoothing when estimating the probabilities, so
+        # figure out the size of the overall vocabulary.
+        for _, corpus in training_set.items():
+            for document in corpus:
+                for field in fields:
+                    for term in self.__get_terms(document.get_field(field, "")):
+                        self.__vocabulary.add_if_absent(term)
 
     def __compute_posteriors(self, training_set, fields) -> None:
         """
         Estimates all conditional probabilities (or, rather, log-probabilities) needed for
         the naive Bayes classifier.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        # Use smoothed estimates. Remember the denominators we used, so that we later know how
+        # to deal with terms we haven't seen for a category.
+        for category, corpus in training_set.items():
+            term_frequencies = Counter(self.__get_terms(" ".join(d.get_field(f, "") for d in corpus for f in fields)))
+            self.__denominators[category] = sum(term_frequencies.values()) + self.__vocabulary.size()
+            self.__posteriors[category] = {term: self.__smooth(frequency, category) for term, frequency in term_frequencies.items()}
+
+    def __smooth(self, frequency: int, category: str) -> float:
+        """
+        Computes a smoothed log-probability, using Lapace add-one smoothing. Assumes that
+        we've already computed the correct fraction denominator to use for the given category.
+        """
+        return math.log((frequency + 1) / self.__denominators[category])
 
     def __get_terms(self, buffer) -> Iterator[str]:
         """
@@ -85,10 +106,14 @@ class NaiveBayesClassifier:
     def get_posterior(self, category: str, term: str) -> float:
         """
         Given a category c and a term t, returns the posterior log-probability log(Pr(t | c)).
+        If the term has not been observed for the current category, use a smoothed estimate.
 
         This is an internal detail having public visibility to facilitate testing.
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        try:
+            return self.__posteriors[category][term]
+        except KeyError:
+            return self.__smooth(0, category)
 
     def classify(self, buffer: str) -> Iterator[Dict[str, Any]]:
         """
@@ -99,4 +124,12 @@ class NaiveBayesClassifier:
         The results yielded back to the client are dictionaries having the keys "score" (float) and
         "category" (str).
         """
-        raise NotImplementedError("You need to implement this as part of the obligatory assignment.")
+        # Only consider terms that occurred in the training set, for any category.
+        terms = [term for term in self.__get_terms(buffer) if term in self.__vocabulary]
+
+        # Compute log-probabilities for each category using priors and posteriors.
+        scores = {category: prior + sum(self.get_posterior(category, term) for term in terms) for category, prior in self.__priors.items()}
+
+        # Emit categories back to the client in sorted order.
+        results = ({"score": score, "category": category} for category, score in sorted(scores.items(), key=lambda pair: pair[1], reverse=True))
+        yield from results
